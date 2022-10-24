8bfb8e632309b4379ced60ac58c78fa1a682e7e1
guitarkeegan <keegananglim@gmail.com>
2022-10-24 10:27:10 -0700

    import chainlink interface

--- contracts/Surpay.sol
+++ contracts/Surpay.sol
@@ -2,18 +2,38 @@
 
 pragma solidity ^0.8.8;
 
+// import AutomationCompatibleInterface
+import "@chainlink/contracts/src/v0.8/interfaces/AutomationCompatibleInterface.sol";
+
 contract Surpay{
     // create storage for surveys and user data
     struct Company{
         string id;
-        Survey[] survey;
+        address companyAddress;
+        Survey[] surveys;
     }
+    /**
+     * @dev Survey will hold the survey ID as well as a mapping for each user address and response data for the survey.
+     */
     struct Survey{
         string id;
-        mapping (address => string) surveyResponces;
+        uint256 payoutAmount;
+        mapping (address => string) surveyUsersAndResponseData;
+        
     }
     
-    /* global vars  */
+    /* state variables  */
     Company[] public companies;
+
+    /* constructor */
+
+    /* events */
+
+    /* functions */
+
+    // function createCompany(){}
+    // function submitUserSurveyData(){}
+    // function createSurvey(){}
+
 
 }
